5472f12cbeb1ed23bdc82e85ed8ccbb3b058a156
Jean-Luc Mourey <moureyjeanluc@gmail.com>
2020-06-06 18:24:12 +0200

    Added code for splitting item stacks with probability of appearance.

--- src/main/java/me/kixstar/dungeons/dungeons/Loot.java
+++ src/main/java/me/kixstar/dungeons/dungeons/Loot.java
@@ -2,6 +2,7 @@ package me.kixstar.dungeons.dungeons;
 
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,10 +16,32 @@ public class Loot {
                 .collect(Collectors.toList());
     }
 
-    public static List<ItemStack> generateLoot(LootConfiguration configuration) {
-        return null;
-    }
-        //uses the probability of the item in the configfuration list to determine if it'll appear in the dungeon
+    private static List<ItemStack> generateLoot(LootConfiguration configuration) {
+        List<ItemStack> finalItemStackList = new ArrayList<>();
+
+        if (Math.random() <= configuration.getChance()) {
+            int itemAmount = configuration.getItemStack().getAmount();
+            int itemsForStack;
+            int maxStackSize = configuration.getMaxStackSize();
+
+            while (itemAmount > 0) {
+                if (itemAmount - maxStackSize >= 0)
+                {
+                    itemsForStack = maxStackSize;
+                } else {
+                    itemsForStack = itemAmount;
+                }
+                finalItemStackList.add(new ItemStack(configuration.getItemStack().getType(), itemsForStack));
+				itemAmount -= maxStackSize;
+            }
+        }
+
+        return finalItemStackList;
+
+        //uses the probability of the item in the configuration list to determine if it'll appear in the dungeon
         //splits the item lists into acceptable stack sizes
         //lootconfigs are for one item type each
+    }
+
+
 }
