e2653dfa792905e84c1053b49667b18ded47c7f8
CodingSeaOtter <CodingSeaOtter@gmail.com>
2020-06-06 08:06:53 -0400

    Multiverse core implementation

--- src/main/java/me/kixstar/dungeons/KixstarDungeons.java
+++ src/main/java/me/kixstar/dungeons/KixstarDungeons.java
@@ -1,24 +1,42 @@
 package me.kixstar.dungeons;
 
+import com.onarandombox.MultiverseCore.MultiverseCore;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KixstarDungeons extends JavaPlugin {
     private static final Plugin instance = Bukkit.getServer().getPluginManager().getPlugin("KixstarDungeons");
-
+    private MultiverseCore multiverse_core;
 
     public void onEnable() {
-        this.setupConfig();
-    }
+        if(!this.setupMultiverse()) {
+            return;
+        }
 
-    protected void setupConfig() {
-        this.getConfig().options().copyDefaults();
-        this.saveConfig();
+        this.setupConfig();
     }
 
     public void onDisable() {
 
+    }
+
+    private void setupConfig() {
+        this.getConfig().options().copyDefaults();
+        this.saveConfig();
+    }
+
+    private boolean setupMultiverse() {
+        Plugin multiverse_plugin = Bukkit.getPluginManager().getPlugin("Multiverse-Core");
+
+        if (null == multiverse_plugin) {
+            this.getLogger().severe("Multiverse-Core is missing and unfortunately, not optional.");
+            this.getServer().getPluginManager().disablePlugin(this);
+
+            return false;
+        }
+
+        return true;
     }
 
     /**
@@ -28,5 +46,13 @@ public class KixstarDungeons extends JavaPlugin {
      */
     public static KixstarDungeons getInstance() {
         return null == instance ? null : (KixstarDungeons) instance;
+    }
+
+    public MultiverseCore getMultiverseCore() {
+        return this.multiverse_core;
+    }
+
+    private void setMultiveseCore(MultiverseCore multiverse_core) {
+        this.multiverse_core = multiverse_core;
     }
 }
