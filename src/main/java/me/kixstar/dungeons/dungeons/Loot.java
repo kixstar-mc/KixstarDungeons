fdc35f41223c37ab1aa96a5b54b4286d8c670f3b
Carlo Field <cfi@bluesky-it.ch>
2020-06-06 16:36:56 +0200

    update to add method wrapper around lists of configurations

--- src/main/java/me/kixstar/dungeons/dungeons/Loot.java
+++ src/main/java/me/kixstar/dungeons/dungeons/Loot.java
@@ -2,11 +2,20 @@ package me.kixstar.dungeons.dungeons;
 
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Loot {
 
     public static List<ItemStack> generateLoot(List<LootConfiguration> configurations) {
+        return configurations.parallelStream()
+                .map(Loot::generateLoot)
+                .flatMap(Collection::parallelStream)
+                .collect(Collectors.toList());
+    }
+
+    public static List<ItemStack> generateLoot(LootConfiguration configuration) {
         return null;
     }
 
