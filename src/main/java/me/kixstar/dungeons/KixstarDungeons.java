36b163b5401bbf0ed5740dc24ac4e7626d0e0219
CodingSeaOtter <CodingSeaOtter@gmail.com>
2020-06-06 07:46:41 -0400

    Instance fetching

--- src/main/java/me/kixstar/dungeons/KixstarDungeons.java
+++ src/main/java/me/kixstar/dungeons/KixstarDungeons.java
@@ -1,13 +1,27 @@
 package me.kixstar.dungeons;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KixstarDungeons extends JavaPlugin {
+    private static final Plugin instance = Bukkit.getServer().getPluginManager().getPlugin("KixstarDungeons");
+
+
     public void onEnable() {
 
     }
 
     public void onDisable() {
 
+    }
+
+    /**
+     * Retrieve the instance of the plugin from Bukkit's Plugin Manager
+     *
+     * @return KixstarDungeons instance
+     */
+    public static KixstarDungeons getInstance() {
+        return null == instance ? null : (KixstarDungeons) instance;
     }
 }
