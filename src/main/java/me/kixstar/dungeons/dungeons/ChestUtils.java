86bd428205e932874f88d77b32f376d9300e8f4a
Leon Dietzel <derlionzockt@gmail.com>
2020-06-07 17:08:42 +0200

    updated item distribution after addressing the changes you asked for

--- src/main/java/me/kixstar/dungeons/dungeons/ChestUtils.java
+++ src/main/java/me/kixstar/dungeons/dungeons/ChestUtils.java
@@ -1,52 +1,53 @@
 package me.kixstar.dungeons.dungeons;
 
 import org.bukkit.block.Chest;
-import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class ChestUtils {
 
     private static final Random random = new Random();
+
     /**
      * distributeItems distributes a list of items into different chests, randomly
+     *
      * @param chests
      * @param items
      */
     static void distributeItems(List<Chest> chests, List<ItemStack> items) {
-        if (chests != null && items != null && !(items.isEmpty())) {
-            for (Chest chest : chests) {
-                List<ItemStack> itemStacks = null;
-                for (int i = 0; i < random.nextInt(items.size()); i++) {
-                    itemStacks.add(items.get(i));
-                    items.remove(i);
-                }
-                distributeItems(chest.getInventory(), itemStacks);
-            }
-        } else {
+        if (chests == null || items == null || items.isEmpty()) {
             return;
+        }
+        for (Chest chest : chests) {
+            List<ItemStack> itemStacks = new ArrayList<>();
+            for (int i = 0; i < random.nextInt(items.size()); i++) {
+                itemStacks.add(items.get(i));
+                items.remove(i);
+            }
+            distributeItems(chest.getInventory(), itemStacks);
         }
     }
 
     /**
      * distributeItems distributes a list of items into random slots of a given inventory
+     *
      * @param inventory
      * @param items
      */
     static void distributeItems(Inventory inventory, List<ItemStack> items) {
-        if (inventory != null && items != null && !(items.isEmpty())) {
-            for (ItemStack item : items) {
-                int ramdomSlot = random.nextInt(27);
-                if (inventory.getItem(ramdomSlot) != null) {
-                    inventory.setItem(ramdomSlot, item);
-                }
-            }
-        } else {
+        if (inventory == null || items == null || items.isEmpty()) {
             return;
+        }
+        for (ItemStack item : items) {
+            int ramdomSlot = random.nextInt(27);
+            while (inventory.getItem(ramdomSlot) != null) {
+                ramdomSlot = random.nextInt(27);
+            }
+            inventory.setItem(ramdomSlot, item);
         }
     }
 }
